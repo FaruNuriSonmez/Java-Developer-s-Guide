a52ba30d825e13d42c6d37215ece67bbfaec8b5b
Faru Nuri Sönmez <info@farunurisonmez.com>
2021-10-28 03:54:57 +0300

    Chapter 11 - Inheritance
    ...
    
    Co-Authored-By: Faru Nuri Sönmez <info@farunurisonmez.com>

---
+++ 11-Inheritance/src/PersonManager.java
@@ -0,0 +1,9 @@
+public class PersonManager {
+    public void List(){
+        System.out.println("Customer Listing...");
+    }
+
+    public void Add(){
+        System.out.println("Customer Added...");
+    }
+}
