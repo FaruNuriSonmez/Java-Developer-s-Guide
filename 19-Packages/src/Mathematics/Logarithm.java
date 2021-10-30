3c5e6c9eaaff8d2acbe23a938f626e49340598e7
Faru Nuri Sönmez <info@farunurisonmez.com>
2021-10-30 13:31:43 +0300

    Chapter 19 - Packages
    ...
    
    Co-Authored-By: Faru Nuri Sönmez <info@farunurisonmez.com>

---
+++ 19-Packages/src/Mathematics/Logarithm.java
@@ -0,0 +1,7 @@
+package Mathematics;
+
+public class Logarithm {
+    public double calculate(){
+        return 1;
+    }
+}
---
+++ 19-Packages/src/Mathematics/Transactions.java
@@ -0,0 +1,7 @@
+package Mathematics;
+
+public class Transactions {
+    public int Addition(int number1, int number2){
+        return number1 + number2;
+    }
+}
