fe03ba6ab93a71b80ce6027f85b7772819b852d5
Faru Nuri Sönmez <info@farunurisonmez.com>
2021-10-31 23:43:53 +0300

    Chapter 22 - Working With Files
    ...
    
    Co-Authored-By: Faru Nuri Sönmez <info@farunurisonmez.com>

--- 22-WorkingWithFiles/src/Main.java
+++ 22-WorkingWithFiles/src/Main.java
@@ -5,6 +5,11 @@ public class Main {
 
     public static void main(String[] args) {
         File file = new File("/Users/farunurisonmez/Desktop/students.txt");
+        createFile(file);
+        readingFileInfo(file);
+    }
+
+    public static void createFile(File file){
         try {
             if (file.createNewFile()){
                 System.out.println("File Created.");
@@ -15,6 +20,16 @@ public class Main {
         }
         catch (IOException e){
             e.printStackTrace();
+        }
+    }
+
+    public static void readingFileInfo(File file){
+        if (file.exists()){
+            System.out.println("File Name : " + file.getName());
+            System.out.println("File Path : " + file.getAbsolutePath());
+            System.out.println("File Size : " + file.length());
+            System.out.println("Is the file writable : " + file.canWrite());
+            System.out.println("Is the file readable : " + file.canRead());
         }
     }
 }
