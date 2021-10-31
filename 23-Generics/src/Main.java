52104e11f1aff65da417b6f6dd74dad30b552c3b
Faru Nuri Sönmez <info@farunurisonmez.com>
2021-11-01 01:00:24 +0300

    Chapter 23 - Generics
    ...
    
    Co-Authored-By: Faru Nuri Sönmez <info@farunurisonmez.com>

---
+++ 23-Generics/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyList<Customer> customerMyList = new MyList<Customer>();
+        customerMyList.add(new Customer());
+    }
+}
