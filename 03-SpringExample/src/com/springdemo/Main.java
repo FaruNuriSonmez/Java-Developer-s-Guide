c564f635e3f74d3cc2af08c9c58d17333182ed14
Faru Nuri Sönmez <info@farunurisonmez.com>
2021-11-04 16:31:38 +0300

    Example 03 - Spring
    ...
    
    Co-Authored-By: Faru Nuri Sönmez <info@farunurisonmez.com>

--- 03-SpringExample/src/com/springdemo/Main.java
+++ 03-SpringExample/src/com/springdemo/Main.java
@@ -1,15 +1,14 @@
-package applicationSpring;
+package com.springdemo;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import com.springdemo.IocConfig;
 
 public class Main {
 
 	public static void main(String[] args) {
 	    
-	    AnnotationConfigApplicationContext context = 
+	   AnnotationConfigApplicationContext context = 
 		    new AnnotationConfigApplicationContext(IocConfig.class);
+	    
 	    ICustomerService customerService = 
 		    context.getBean("service",ICustomerService.class);
 	    
