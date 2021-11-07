d27c61a83df2df52ee6b8a35d20df04ac8cf7f17
Faru Nuri Sönmez <info@farunurisonmez.com>
2021-11-07 10:26:48 +0300

    Example 06 - Spring Boot
    ...
    
    Co-Authored-By: Faru Nuri Sönmez <info@farunurisonmez.com>

--- 06-SpringBootExample/src/main/java/com/spring/example/apiExample/ProductsController.java
+++ 06-SpringBootExample/src/main/java/com/spring/example/apiExample/ProductsController.java
@@ -1,5 +1,18 @@
 package com.spring.example.apiExample;
 
-public class ProductsController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class ProductsController {
+    
+    @GetMapping("/")
+    public String get() {
+	return "Laptop";
+    }
+    
+    @GetMapping("/products")
+    public String get2() {
+	return "Laptop2";
+    }
 }
